665880356d31ce03a87a1cac61209ab87c7e3ee7
marshal <marshalalakkadan@gmail.com>
2023-01-10 18:36:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import DescriptionCard from './Components/DescriptionCard/DescriptionCard';
+import { ReactComponent as GoShop } from './assets/goshop.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DescriptionCard GoShop={GoShop} title={'lOREIPSUM'} description1={`Please enter your login information to`}
+                        description2={'sign in to your account'}/>
     </div>
   );
 }
