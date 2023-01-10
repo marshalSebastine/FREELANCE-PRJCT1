4ff7e19b4217f5d33e030591e9dae9312b799bd5
marshal <marshalalakkadan@gmail.com>
2023-01-10 19:43:49 +0530

    base components set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import DescriptionCard from './Components/DescriptionCard/DescriptionCard';
 import { ReactComponent as GoShop } from './assets/goshop.svg';
+import LogoCard from './Components/LogoCard/LogoCard';
 
 function App() {
   return (
     <div className="App">
       <DescriptionCard GoShop={GoShop} title={'lOREIPSUM'} description1={`Please enter your login information to`}
                         description2={'sign in to your account'}/>
+      <LogoCard/>
     </div>
   );
 }
--- src/Components/LogoCard/LogoCard.js
+++ src/Components/LogoCard/LogoCard.js
@@ -3,7 +3,9 @@ import './LogoCard.styles.css';
 const LogoCard = () => {
     return(
         <div className='LogoCard'>
-
+            <div className='LogoHeader'>
+                <h1 className='Logo'>{'LOGO'}</h1>
+            </div>
         </div>
     )
 }
